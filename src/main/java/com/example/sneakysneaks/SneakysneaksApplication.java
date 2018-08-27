fbe0f106f8384ef980b8d82565550dedd971e152
CUNY Tech Prep <cunytechprep1@CUNYs-MacBook-Pro.local>
2018-08-27 14:33:29 -0400

    fix swagger config

--- src/main/java/com/example/sneakysneaks/SneakysneaksApplication.java
+++ src/main/java/com/example/sneakysneaks/SneakysneaksApplication.java
@@ -1,9 +1,12 @@
 package com.example.sneakysneaks;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -14,20 +17,22 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @SpringBootApplication
 @EnableSwagger2
 public class SneakysneaksApplication {
+	private static final Set<String> DEFAULT_PRODUCES_AND_CONSUMES = new HashSet<String>(Arrays.asList("application/json"));
 
 	public static void main(String[] args) {
 		SpringApplication.run(SneakysneaksApplication.class, args);
 	}
+	
 
 	@Bean
 	public Docket api() {
 		return new Docket(DocumentationType.SWAGGER_2)
-				// .consumes(DEFAULT_PRODUCES_AND_CONSUMES)
+				 .consumes(DEFAULT_PRODUCES_AND_CONSUMES)
 				.select()
-				.apis(RequestHandlerSelectors.any()) 
+				//.apis(RequestHandlerSelectors.any()) 
 				// "any" will make documentation for entire API available
 				// through Swagger
-				// .apis(RequestHandlerSelectors.basePackage("com.example.sneakysneaks.controller"))
+				 .apis(RequestHandlerSelectors.basePackage("com.example.sneakysneaks.controller"))
 				.paths(PathSelectors.any())
 				.build();
 	}
