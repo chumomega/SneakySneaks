a147c6283e443ac73f263da06dafea2f5a3cdcf6
CUNY Tech Prep <cunytechprep1@CUNYs-MBP.fios-router.home>
2018-08-27 13:32:07 -0400

    change package to look in for controllers - swagger

--- src/main/java/com/example/sneakysneaks/SneakysneaksApplication.java
+++ src/main/java/com/example/sneakysneaks/SneakysneaksApplication.java
@@ -2,10 +2,6 @@ package com.example.sneakysneaks;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 public class SneakysneaksApplication{
@@ -14,13 +10,4 @@ public class SneakysneaksApplication{
 		SpringApplication.run(SneakysneaksApplication.class, args);
 	}
 	
-	@Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurerAdapter() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/greeting-javaconfig").allowedOrigins("https://safe-anchorage-62211.herokuapp.com");
-            }
-        };
-    }
 }
