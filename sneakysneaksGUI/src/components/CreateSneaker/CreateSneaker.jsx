6b8ee7e275476b382c59763623952ec5de41a5df
Chuma Okoro <cokoro@etsy.com>
2020-03-19 23:58:56 -0400

    add image urls to db after created

--- sneakysneaksGUI/src/components/CreateSneaker/CreateSneaker.jsx
+++ sneakysneaksGUI/src/components/CreateSneaker/CreateSneaker.jsx
@@ -27,21 +27,50 @@ class CreateSneaker extends Component {
         var filepath = ReactDOM.findDOMNode(this.refs['picture']).value.trim();
         var storageRef = this.props.storage_service.ref().child(filepath);
         
-        console.log("this is the filepath: " + filepath)
-        storageRef.put(selectedFile).then(function(snapshot) {
-            console.log('Uploaded a blob or file!');
+        var upload_task = storageRef.put(selectedFile)
+        var this_object = this
+
+        // Register three observers:
+        // 1. 'state_changed' observer, called any time the state changes
+        // 2. Error observer, called on failure
+        // 3. Completion observer, called on successful completion
+        upload_task.on('state_changed', function(snapshot){
+            // Observe state change events such as progress, pause, and resume
+            // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+            var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+            // console.log('Upload is ' + progress + '% done');
+            // switch (snapshot.state) {
+            //     case firebase.storage.TaskState.PAUSED: // or 'paused'
+            //         console.log('Upload is paused');
+            //         break;
+            //     case firebase.storage.TaskState.RUNNING: // or 'running'
+            //         console.log('Upload is running');
+            //         break;
+            // }
+        }, function(error) {
+            // Handle unsuccessful uploads
+        }, function() {
+            // Handle successful uploads on complete
+            // For instance, get the download URL: https://firebasestorage.googleapis.com/...
+            upload_task.snapshot.ref.getDownloadURL().then(function(downloadURL) {
+                //TODO - create new sneaker out of properties in the state
+                this_object.props.attributes.forEach(attribute => {
+                    if (attribute == 'picture') {
+                        newSneaker[attribute] = downloadURL;
+                    } else {
+                        newSneaker[attribute] = ReactDOM.findDOMNode(this_object.refs[attribute]).value.trim();
+                    }
+                });
+                this_object.props.onCreate(newSneaker);
+
+                // clear out the dialog's inputs
+                this_object.props.attributes.forEach(attribute => {
+                    ReactDOM.findDOMNode(this_object.refs[attribute]).value = '';
+                });
+            })
+            .then()
         })
 
-        //TODO - create new sneaker out of properties in the state
-        this.props.attributes.forEach(attribute => {
-            newSneaker[attribute] = ReactDOM.findDOMNode(this.refs[attribute]).value.trim();
-        });
-        this.props.onCreate(newSneaker);
-
-        // clear out the dialog's inputs
-        this.props.attributes.forEach(attribute => {
-            ReactDOM.findDOMNode(this.refs[attribute]).value = '';
-        });
 
         // Navigate away from the dialog to hide it.
         window.location = "#";
